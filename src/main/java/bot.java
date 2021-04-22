be4f8da50c49398bcaebddb07783254017040833
AmmoniumX <75765000+AmmoniumX@users.noreply.github.com>
2021-04-21 19:37:50 -0600

    replaced global "defaultRole" feature with message-specific "removalRole" feature.

--- src/main/java/bot.java
+++ src/main/java/bot.java
@@ -16,8 +16,8 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class bot extends ListenerAdapter{
-    private static String defaultRoleId = null;
     private static HashMap<String, HashMap<String, String>> messageIdToEmojiAndRoleId = new HashMap<>();
+    private static HashMap<String, String> messageIdToRemovalRoleId = new HashMap<>();
 
     public static void main(String[] args) throws LoginException, InterruptedException, IOException {
         JDA jda = JDABuilder.createDefault("ODM0NTg3OTE1NDI1ODA4NDI0.YIDEkA.3dU_OaTKLmHe2ulOFeu7jXC0_QE").build().awaitReady();
@@ -61,29 +61,39 @@ public class bot extends ListenerAdapter{
                 ois.close();
                 fis.close();
 
+                String activemessagesString = messageIdToEmojiAndRoleId.keySet().toString();
+                System.out.println("Active messages: " + activemessagesString);
+
             } catch (Exception ex){
                 messageIdToEmojiAndRoleId = new HashMap<>();
                 ex.printStackTrace();
             }
         }
 
-        // now do it again for defaultRoleId.txt
-
-        String defaultRolePath = "./data/defaultRoleId.txt";
-        File defaultRoleFile = new File(defaultRolePath);
-        if (!defaultRoleFile.exists()){
-            System.out.println("Could not find defaultRoleId.txt, creating it...");
-            boolean fileCreateSuccess = defaultRoleFile.createNewFile();
+        // look for messageIdToRemovalRoleId.txt, if it doesn't exist, create it
+        String filePath2 = "./data/messageIdToRemovalRoleId.txt";
+        File messageIdToRemovalRoleIdFile = new File(filePath2);
+        if (!messageIdToRemovalRoleIdFile.exists()){
+            System.out.println("Could not find messageIdToRemovalRoleId.txt, creating it...");
+            boolean fileCreateSuccess = messageIdToRemovalRoleIdFile.createNewFile();
             System.out.println(fileCreateSuccess);
-        } else if (defaultRoleFile.length() > 0){
-            // Since defaultRoleId is just a string, the process is much simpler
+
+        } else if (messageIdToRemovalRoleIdFile.length() > 0){
+            // now we can actually read the file
             try {
-                FileInputStream fis = new FileInputStream(defaultRoleFile);
-                defaultRoleId = new String(fis.readAllBytes());
-                System.out.println("Default role ID: " + defaultRoleId);
+                FileInputStream fis = new FileInputStream(messageIdToRemovalRoleIdFile);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                //noinspection unchecked
+                messageIdToRemovalRoleId = (HashMap<String, String>) ois.readObject();
+                // ignore warning, can't be done much abt it
+
+                ois.close();
                 fis.close();
+
+                System.out.println("Loaded " + messageIdToRemovalRoleId.size() + " objects from messageIdToRemovalRoleId.txt");
+
             } catch (Exception ex){
-                defaultRoleId = null;
+                messageIdToRemovalRoleId = new HashMap<>();
                 ex.printStackTrace();
             }
         }
@@ -110,10 +120,13 @@ public class bot extends ListenerAdapter{
                 String helpString = "help: shows this message. \n" +
                         "setreactionrole [messageId] [emoji] [@role]: sets [emoji] in message [messageId] to give role [" +
                         "@role] when reacted to. Doesn't accept custom emotes! \n" +
-                        "setdefaultrole [@role]: sets [@role] to be removed when reacting. Doesn't actually give the role " +
                         "when joining! \n" +
                         "listactivemessages: shows a list of all active messages by their Id. \n" +
-                        "removeactivemessage [messageId]: deletes all active reaction roles for [messageId]."
+                        "(extra: messageIds have a '~' next to them if they are associated with a removalRole\n" +
+                        "removeactivemessage [messageId]: deletes reaction roles for [messageId] and removal roles" +
+                        "if present. \n" +
+                        "setRemovalRole [messageId] [@role]: sets role to be removed when reacting to [messageId].\n" +
+                        "removeRemovalRole [messageId]: unassigns removal role from [messageId] if present."
                         ;
 
                 e.getChannel().sendMessage(helpString).queue();
@@ -136,7 +149,7 @@ public class bot extends ListenerAdapter{
                 String srrChannelId = srrFullMessageId.substring(0, 18);
                 System.out.println("Channel ID: " + srrChannelId + ", message ID: " + srrMessageId);
                 String reactionEmoji = message[2];
-                java.lang.String roleId = message[3].substring(3, message[3].length() - 1);
+                String roleId = message[3].substring(3, message[3].length() - 1);
                 Role reactionRole = e.getGuild().getRoleById(roleId);
 
                 if (reactionRole == null){
@@ -187,45 +200,22 @@ public class bot extends ListenerAdapter{
                 }
                 break;
 
-            case "setdefaultrole":
-                if (!e.getMember().getPermissions().contains(Permission.ADMINISTRATOR)){
-                    e.getChannel().sendMessage("You don't have enough permissions to do this.").queue();
-                    return;
-                }
-                if (message.length < 2) {
-                    e.getChannel().sendMessage("Not enough arguments, (1) needed").queue();
-                    return;
-                }
-                defaultRoleId = message[1].substring(3, message[1].length() - 1);
-                Role defaultRole = e.getGuild().getRoleById(defaultRoleId);
-
-                if (defaultRole == null) {
-                    e.getChannel().sendMessage("Could not recognize role. Was it written correctly?").queue();
-                    return;
-                }
-
-                e.getChannel().sendMessage("Set " + defaultRole.getAsMention() + " as default role").queue();
-
-                // save defaultRoleId to the file
-                String defaultRolePath = "./data/defaultRoleId.txt";
-                try {
-                    FileOutputStream fos = new FileOutputStream(defaultRolePath);
-                    fos.write(defaultRoleId.getBytes());
-                    fos.close();
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                    System.out.println("Couldn't save defaultRoleId.txt!");
-                }
-                break;
-
             case "listactivemessages":
                 if (!e.getMember().getPermissions().contains(Permission.ADMINISTRATOR)){
                     e.getChannel().sendMessage("You don't have enough permissions to do this.").queue();
                     return;
                 }
-                String activemessagesString = messageIdToEmojiAndRoleId.keySet().toString();
-                System.out.println(activemessagesString);
-                e.getChannel().sendMessage(activemessagesString).queue();
+                StringBuilder activeMessagesList = new StringBuilder();
+                for (String activeMessage: messageIdToEmojiAndRoleId.keySet()) {
+                    activeMessagesList.append(activeMessage);
+                    if (messageIdToRemovalRoleId.containsKey(activeMessage)) {
+                        activeMessagesList.append("~");
+                    }
+                    activeMessagesList.append("\n");
+                }
+                e.getChannel().sendMessage(activeMessagesList.toString()).queue();
+                System.out.println(activeMessagesList.toString());
+
                 break;
 
             case "removeactivemessage":
@@ -246,6 +236,9 @@ public class bot extends ListenerAdapter{
                 }
 
                 messageIdToEmojiAndRoleId.remove(ramFullMessageId);
+
+                messageIdToRemovalRoleId.remove(ramFullMessageId);
+                
                 e.getChannel().sendMessage("Successfully removed from active messages").queue();
 
                 // save messageIdToEmojiToRoleId to the file
@@ -261,6 +254,104 @@ public class bot extends ListenerAdapter{
                 } catch (Exception exception) {
                     exception.printStackTrace();
                     System.out.println("Couldn't save messageIdToEmojiToRoleId.txt!");
+                }
+
+                // save messageIdToRemovalRoleId to the file
+                String filePath2 = "./data/messageIdToRemovalRoleId.txt";
+                File messageIdToRemovalRoleIdFile = new File(filePath2);
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(messageIdToRemovalRoleIdFile, false);
+                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+                    oos.writeObject(messageIdToRemovalRoleIdFile);
+                    oos.close();
+                    fos.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    System.out.println("Couldn't save messageIdToRemovalRoleId.txt!");
+                }
+                
+                break;
+
+            case "setremovalrole":
+                if (!e.getMember().getPermissions().contains(Permission.ADMINISTRATOR)){
+                    e.getChannel().sendMessage("You don't have enough permissions to do this.").queue();
+                    return;
+                }
+                if(message.length < 3) {
+                    e.getChannel().sendMessage("Not enough arguments, (2) needed").queue();
+                    return;
+                }
+
+                String rrMessageId = message[1];
+                String rrRoleId = message[2].substring(3, message[2].length() - 1);
+                Role removalRole = e.getGuild().getRoleById(rrRoleId);
+
+                if (removalRole == null) {
+                    e.getChannel().sendMessage("Removal role could not be read. Was it written correctly?").queue();
+                    return;
+                }
+
+                if (!messageIdToEmojiAndRoleId.containsKey(rrMessageId)) {
+                    e.getChannel().sendMessage("That message isn't registered as an active message, try ;setreactionrole first.").queue();
+                    return;
+                }
+
+
+                messageIdToRemovalRoleId.remove(rrMessageId);
+                messageIdToRemovalRoleId.put(rrMessageId, rrRoleId);
+
+                e.getChannel().sendMessage("Successfully associated active message " + rrMessageId + "to removal role " +
+                removalRole.getAsMention()).queue();
+
+                // save messageIdToRemovalRoleId to the file
+                filePath2 = "./data/messageIdToRemovalRoleId.txt";
+                messageIdToRemovalRoleIdFile = new File(filePath2);
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(messageIdToRemovalRoleIdFile, false);
+                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+                    oos.writeObject(messageIdToRemovalRoleIdFile);
+                    oos.close();
+                    fos.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    System.out.println("Couldn't save messageIdToRemovalRoleId.txt!");
+                }
+                break;
+
+            case "removeremovalrole":
+                if (!e.getMember().getPermissions().contains(Permission.ADMINISTRATOR)){
+                    e.getChannel().sendMessage("You don't have enough permissions to do this.").queue();
+                    return;
+                }
+                if (message.length < 2) {
+                    e.getChannel().sendMessage("Not enough arguments, (1) needed").queue();
+                    return;
+                }
+
+                String removalMessageId = message[1];
+
+                if (!messageIdToRemovalRoleId.containsKey(removalMessageId)) {
+                    e.getChannel().sendMessage("That message has no removal roles associated with it.").queue();
+                    return;
+                }
+
+                messageIdToRemovalRoleId.remove(removalMessageId);
+
+                // save messageIdToRemovalRoleId to the file
+                filePath2 = "./data/messageIdToRemovalRoleId.txt";
+                messageIdToRemovalRoleIdFile = new File(filePath2);
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(messageIdToRemovalRoleIdFile, false);
+                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+                    oos.writeObject(messageIdToRemovalRoleIdFile);
+                    oos.close();
+                    fos.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    System.out.println("Couldn't save messageIdToRemovalRoleId.txt!");
                 }
                 break;
 
@@ -308,13 +399,16 @@ public class bot extends ListenerAdapter{
         e.getGuild().addRoleToMember(e.getMember(), roleToGive).queue();
         System.out.println("Successfully added role " + roleToGive.getName() + " to " + e.getMember().getEffectiveName());
 
-        if (defaultRoleId != null) {
-            Role defaultRole = e.getGuild().getRoleById(defaultRoleId);
-            assert defaultRole != null;
-            if (e.getMember().getRoles().contains(defaultRole)) {
-                e.getGuild().removeRoleFromMember(e.getMember(), defaultRole).queue();
-                System.out.println("Successfully removed role " + defaultRole.getName() + " from " + e.getMember().getEffectiveName());
+        if(messageIdToRemovalRoleId.containsKey(fullMessageId)) {
+            String removalRoleId = messageIdToRemovalRoleId.get(fullMessageId);
+            Role removalRole = e.getGuild().getRoleById(removalRoleId);
+            assert removalRole != null;
+
+            if (e.getMember().getRoles().contains(removalRole)) {
+                e.getGuild().removeRoleFromMember(e.getMember(), removalRole).queue();
+                System.out.println("Removed role " + removalRoleId + " from " + e.getMember().getEffectiveName());
             }
+
         }
 
         e.getReaction().removeReaction(e.getUser()).queue();
